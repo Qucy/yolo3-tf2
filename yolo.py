396e91a5b8d9856437f0384debcd2bea3d1b6b1d
Qucy <tracyqucy@gmail.com>
2021-12-28 21:51:14 +0800

    remove weights

--- yolo.py
+++ yolo.py
@@ -24,7 +24,7 @@ class YOLO(object):
         #   验证集损失较低不代表mAP较高，仅代表该权值在验证集上泛化性能较好。
         #   如果出现shape不匹配，同时要注意训练时的model_path和classes_path参数的修改
         #--------------------------------------------------------------------------#
-        "model_path"        : 'model_data/yolo_weights.h5',
+        "model_path"        : 'logs/self_trained_yolo_weights.h5',
         "classes_path"      : 'model_data/coco_classes.txt',
         #---------------------------------------------------------------------#
         #   anchors_path代表先验框对应的txt文件，一般不修改。
